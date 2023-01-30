f63c05474024f1aee3ad1ad7db53112f0e686315
tk-yoshimura <yoshimura8192@gmail.com>
2023-01-30 17:24:24 +0900

    fix sum_table

--- MultiPrecisionCurveFitting/SumTable.cs
+++ MultiPrecisionCurveFitting/SumTable.cs
@@ -2,7 +2,7 @@
 using MultiPrecisionAlgebra;
 
 namespace MultiPrecisionCurveFitting {
-    internal class SumTable<N> where N : struct, IConstant {
+    public class SumTable<N> where N : struct, IConstant {
         private readonly List<Vector<N>> xs = new(), ys = new();
         private Dictionary<(int xn, int yn), MultiPrecision<N>> table;
 
@@ -27,35 +27,37 @@ namespace MultiPrecisionCurveFitting {
                 }
 
                 for (int i = xs.Count; i < xn; i++) {
-                    xs.Add(xs[0] * xs[^1]);
+                    int xn0 = (i + 1) / 2 - 1, xn1 = i - xn0 - 1;
+
+                    xs.Add(xs[xn0] * xs[xn1]);
                 }
 
                 for (int i = ys.Count; i < yn; i++) {
-                    ys.Add(ys[0] * ys[^1]);
+                    int yn0 = (i + 1) / 2 - 1, yn1 = i - yn0 - 1;
+                    
+                    ys.Add(ys[yn0] * ys[yn1]);
                 }
 
                 if (!table.ContainsKey((xn, yn))) {
+                    MultiPrecision<N> s;
+                    
                     if (xn > 0 && yn > 0) {
                         Vector<N> x = xs[xn - 1], y = ys[yn - 1];
 
-                        MultiPrecision<N> s = w is null ? (x * y).Sum : (x * y * w).Sum;
-
-                        table.Add((xn, yn), s);
+                        s = w is null ? (x * y).Sum : (x * y * w).Sum;
                     }
                     else if (xn > 0) {
                         Vector<N> x = xs[xn - 1];
 
-                        MultiPrecision<N> s = w is null ? x.Sum : (x * w).Sum;
-
-                        table.Add((xn, yn), s);
+                        s = w is null ? x.Sum : (x * w).Sum;
                     }
                     else {
                         Vector<N> y = ys[yn - 1];
 
-                        MultiPrecision<N> s = w is null ? y.Sum : (y * w).Sum;
-
-                        table.Add((xn, yn), s);
+                        s = w is null ? y.Sum : (y * w).Sum;
                     }
+
+                    table.Add((xn, yn), s);
                 }
 
                 return table[(xn, yn)];
--- MultiPrecisionCurveFittingTest/SumTableTests.cs
+++ MultiPrecisionCurveFittingTest/SumTableTests.cs
@@ -7,53 +7,40 @@ namespace MultiPrecisionCurveFitting.Tests {
     public class SumTableTests {
         [TestMethod()]
         public void IndexerTest() {
-            SumTable<Pow2.N4> table = new(new Vector<Pow2.N4>(2, 3, 5, 7), new Vector<Pow2.N4>(11, 13, 17, 19));
+            (MultiPrecision<Pow2.N4> x, MultiPrecision<Pow2.N4> y)[] vs = new (MultiPrecision<Pow2.N4>, MultiPrecision<Pow2.N4>)[] {
+                (2, 11), (3, 13), (5, 17), (7, 19)
+            };
+            MultiPrecision<Pow2.N4> s(int xn, int yn) {
+                return vs.Select(v => MultiPrecision<Pow2.N4>.Pow(v.x, xn) * MultiPrecision<Pow2.N4>.Pow(v.y, yn)).Sum();
+            };
 
-            Assert.AreEqual(4, table[0, 0]);
-            Assert.AreEqual(2 + 3 + 5 + 7, table[1, 0]);
-            Assert.AreEqual(2 * 2 + 3 * 3 + 5 * 5 + 7 * 7, table[2, 0]);
-            Assert.AreEqual(2 * 2 * 2 + 3 * 3 * 3 + 5 * 5 * 5 + 7 * 7 * 7, table[3, 0]);
+            SumTable<Pow2.N4> table = new(vs.Select(v => v.x).ToArray(), vs.Select(v => v.y).ToArray());
 
-            Assert.AreEqual(11 + 13 + 17 + 19, table[0, 1]);
-            Assert.AreEqual(2 * 11 + 3 * 13 + 5 * 17 + 7 * 19, table[1, 1]);
-            Assert.AreEqual(2 * 2 * 11 + 3 * 3 * 13 + 5 * 5 * 17 + 7 * 7 * 19, table[2, 1]);
-            Assert.AreEqual(2 * 2 * 2 * 11 + 3 * 3 * 3 * 13 + 5 * 5 * 5 * 17 + 7 * 7 * 7 * 19, table[3, 1]);
-
-            Assert.AreEqual(11 * 11 + 13 * 13 + 17 * 17 + 19 * 19, table[0, 2]);
-            Assert.AreEqual(2 * 11 * 11 + 3 * 13 * 13 + 5 * 17 * 17 + 7 * 19 * 19, table[1, 2]);
-            Assert.AreEqual(2 * 2 * 11 * 11 + 3 * 3 * 13 * 13 + 5 * 5 * 17 * 17 + 7 * 7 * 19 * 19, table[2, 2]);
-            Assert.AreEqual(2 * 2 * 2 * 11 * 11 + 3 * 3 * 3 * 13 * 13 + 5 * 5 * 5 * 17 * 17 + 7 * 7 * 7 * 19 * 19, table[3, 2]);
-
-            Assert.AreEqual(11 * 11 * 11 + 13 * 13 * 13 + 17 * 17 * 17 + 19 * 19 * 19, table[0, 3]);
-            Assert.AreEqual(2 * 11 * 11 * 11 + 3 * 13 * 13 * 13 + 5 * 17 * 17 * 17 + 7 * 19 * 19 * 19, table[1, 3]);
-            Assert.AreEqual(2 * 2 * 11 * 11 * 11 + 3 * 3 * 13 * 13 * 13 + 5 * 5 * 17 * 17 * 17 + 7 * 7 * 19 * 19 * 19, table[2, 3]);
-            Assert.AreEqual(2 * 2 * 2 * 11 * 11 * 11 + 3 * 3 * 3 * 13 * 13 * 13 + 5 * 5 * 5 * 17 * 17 * 17 + 7 * 7 * 7 * 19 * 19 * 19, table[3, 3]);
+            for (int i = 0; i <= 16; i++) {
+                for (int j = 0; j <= i; j++) {
+                    Assert.AreEqual(s(i, j), table[i, j]);
+                    Assert.AreEqual(s(j, i), table[j, i]);
+                }
+            }
         }
 
         [TestMethod()]
         public void ReverseIndexerTest() {
-            SumTable<Pow2.N4> table = new(new Vector<Pow2.N4>(2, 3, 5, 7), new Vector<Pow2.N4>(11, 13, 17, 19));
+            (MultiPrecision<Pow2.N4> x, MultiPrecision<Pow2.N4> y)[] vs = new (MultiPrecision<Pow2.N4>, MultiPrecision<Pow2.N4>)[] {
+                (2, 11), (3, 13), (5, 17), (7, 19)
+            };
+            MultiPrecision<Pow2.N4> s(int xn, int yn) {
+                return vs.Select(v => MultiPrecision<Pow2.N4>.Pow(v.x, xn) * MultiPrecision<Pow2.N4>.Pow(v.y, yn)).Sum();
+            };
 
-            Assert.AreEqual(2 * 2 * 2 * 11 * 11 * 11 + 3 * 3 * 3 * 13 * 13 * 13 + 5 * 5 * 5 * 17 * 17 * 17 + 7 * 7 * 7 * 19 * 19 * 19, table[3, 3]);
-            Assert.AreEqual(2 * 2 * 11 * 11 * 11 + 3 * 3 * 13 * 13 * 13 + 5 * 5 * 17 * 17 * 17 + 7 * 7 * 19 * 19 * 19, table[2, 3]);
-            Assert.AreEqual(2 * 11 * 11 * 11 + 3 * 13 * 13 * 13 + 5 * 17 * 17 * 17 + 7 * 19 * 19 * 19, table[1, 3]);
-            Assert.AreEqual(11 * 11 * 11 + 13 * 13 * 13 + 17 * 17 * 17 + 19 * 19 * 19, table[0, 3]);
+            SumTable<Pow2.N4> table = new(vs.Select(v => v.x).ToArray(), vs.Select(v => v.y).ToArray());
 
-            Assert.AreEqual(2 * 2 * 2 * 11 * 11 + 3 * 3 * 3 * 13 * 13 + 5 * 5 * 5 * 17 * 17 + 7 * 7 * 7 * 19 * 19, table[3, 2]);
-            Assert.AreEqual(2 * 2 * 11 * 11 + 3 * 3 * 13 * 13 + 5 * 5 * 17 * 17 + 7 * 7 * 19 * 19, table[2, 2]);
-            Assert.AreEqual(2 * 11 * 11 + 3 * 13 * 13 + 5 * 17 * 17 + 7 * 19 * 19, table[1, 2]);
-            Assert.AreEqual(11 * 11 + 13 * 13 + 17 * 17 + 19 * 19, table[0, 2]);
-
-            Assert.AreEqual(2 * 2 * 2 * 11 + 3 * 3 * 3 * 13 + 5 * 5 * 5 * 17 + 7 * 7 * 7 * 19, table[3, 1]);
-            Assert.AreEqual(2 * 2 * 11 + 3 * 3 * 13 + 5 * 5 * 17 + 7 * 7 * 19, table[2, 1]);
-            Assert.AreEqual(2 * 11 + 3 * 13 + 5 * 17 + 7 * 19, table[1, 1]);
-            Assert.AreEqual(11 + 13 + 17 + 19, table[0, 1]);
-
-            Assert.AreEqual(2 * 2 * 2 + 3 * 3 * 3 + 5 * 5 * 5 + 7 * 7 * 7, table[3, 0]);
-            Assert.AreEqual(2 * 2 + 3 * 3 + 5 * 5 + 7 * 7, table[2, 0]);
-            Assert.AreEqual(2 + 3 + 5 + 7, table[1, 0]);
-            Assert.AreEqual(4, table[0, 0]);
-
+            for (int i = 16; i >= 0; i--) {
+                for (int j = i; j >= 0; j--) {
+                    Assert.AreEqual(s(i, j), table[i, j]);
+                    Assert.AreEqual(s(j, i), table[j, i]);
+                }
+            }
         }
     }
 }
